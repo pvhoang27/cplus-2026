ea73d0ab9a28bd58726eb40aac6aca8dbf1538ca
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-01-26 21:15:56 +0700

    update

--- 13-CPP0112-Khoang cach.cpp
+++ 13-CPP0112-Khoang cach.cpp
@@ -4,9 +4,9 @@ using namespace std;
 int main(){
 	int t ; cin >> t;
 	while(t--){
-		int x1 , y1 , x2, y2;
+		double x1 , y1 , x2, y2;
 		cin >> x1 >> y1 >> x2 >> y2;
-		float  kc  = sqrt((x2 - x1) * (x2 - x1) 
+		double   kc  = sqrt((x2 - x1) * (x2 - x1) 
                       + (y2 - y1) * (y2 - y1));
     	cout << fixed << setprecision(4) << kc  << endl;
 	}
