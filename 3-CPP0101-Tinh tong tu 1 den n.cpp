4421da8f9493c5f60af426a33a5d56fc1a9fff57
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-01-26 19:02:57 +0700

    update

---
+++ 3-CPP0101-Tinh tong tu 1 den n.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int t ; cin >> t;
+	while(t--){
+		long long a ; cin >> a;
+		long long sum = a *(a + 1 ) /2;
+		cout<< sum ;
+		cout <<endl;
+		
+	}
+}
